507bbddbd86259e0f33c8f2074d7950bb62a4a2b
devshahoriar <shuvo1556@gmail.com>
2025-07-29 16:25:40 +0600

    add metadata on product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,7 +2,8 @@ import ProductJsonLd from "@/components/shared/ProductJsonLd";
 import { api, HydrateClient } from "@/trpc/server";
 import { ErrorBackButton, FullPage } from "./client";
 import { fetchProduct } from "@/server/services/product.services";
-
+import type { Metadata } from "next";
+import type { ResolvingMetadata } from "next";
 
 // Generate static params for all products
 export async function generateStaticParams() {
@@ -10,6 +11,39 @@ export async function generateStaticParams() {
   return products.map((product) => ({
     id: product.id.toString(),
   }));
+}
+
+type Props = {
+  params: { id: string };
+  searchParams: Record<string, string | string[] | undefined>;
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const { id } = params;
+  // Fetch product data using your service
+  const product = await api.product.getById({ id: parseInt(id) });
+
+  const previousImages = (await parent).openGraph?.images ?? [];
+
+  if (!product) {
+    return {
+      title: "Product Not Found",
+      description: "The product you are looking for does not exist.",
+    };
+  }
+
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      images: [product.image, ...previousImages],
+      title: product.title,
+      description: product.description,
+    },
+  };
 }
 
 export default async function ProductPage({
@@ -21,7 +55,6 @@ export default async function ProductPage({
   const product = await api.product.getById({
     id: parseInt(id),
   });
-
 
   if (!product) {
     return (
