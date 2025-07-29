79f546dad82a010b070374959dd77451e661776b
devshahoriar <shuvo1556@gmail.com>
2025-07-29 15:45:19 +0600

    test again to confirm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { FeaturedProducts } from "@/components/pages/home/featured-products";
 import { HeroSection } from "@/components/pages/home/hero-section";
 import { api, HydrateClient } from "@/trpc/server";
-import type { Product } from '@/types';
-import axios from 'axios';
+import type { Product } from "@/types";
+import axios from "axios";
 
 export default async function Home() {
   void (await api.product.getFeatured.prefetch());
-   const products =
-    (await axios.get<Product[]>("https://fakestoreapi.com/products")).data
-    console.log(products)
+  const products = await axios.get<Product[]>(
+    "https://fakestoreapi.com/products",
+  );
+  console.log(products);
   return (
     <HydrateClient>
       <div className="min-h-screen">
